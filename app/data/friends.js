1ef0b4e8e551b47855f7e7529015ef5338a41561
unknown <katie.albee@gmail.com>
2018-06-13 15:16:22 -0500

    I should learn to commit more often

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [ {
+    "name" : "Nosferatu",
+    "photo": "https://i.pinimg.com/originals/13/3a/27/133a27485721e27fe8237c4804b252ea.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "The Mummy",
+    "photo" : "http://www.bfi.org.uk/sites/bfi.org.uk/files/styles/full/public/image/mummy-the-1932-003-mummy-sarcophagus-00n-d91.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "The Blob",
+    "photo" : "http://www.nick.co.uk/gsp/international/games/sony-hotelt2-what-monster-are-you/en/images/step7/avatar1.png",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Wolf Man",
+    "photo" : "https://theredlist.com/media/database/films/cinema/1940/the-wolf-man/011-the-wolf-man-theredlist.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Invisible Man",
+    "photo" : "https://hollywoodhatesme.files.wordpress.com/2012/07/invisible-man.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Frankenstein's Monster",
+    "photo" : "https://i.giphy.com/Jg779wPIWLSw0.gif",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Dracula",
+    "photo" : "https://marruda3.files.wordpress.com/2017/10/bela-lugosi-dracula.gif",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Bride of Frankenstein",
+    "photo" : "http://bloody-disgusting.com/wp-content/uploads/2015/11/Bride-of-Frankenstein.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Igor",
+    "photo" : "https://i.pinimg.com/736x/d9/5a/f7/d95af768f01311fbea3ead72ff6f45e6--marty-feldman-young-frankenstein.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+
+}];
+
+module.exports = friends;
